4ee729c1a0884f0f2be57b121cec37ae13514723
apiscevs <aleksejs.piscevs@gmail.com>
2022-02-20 14:41:30 +0100

    AL-6: back to original

--- git-learn/Program.cs
+++ git-learn/Program.cs
@@ -1,7 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
-Console.WriteLine("Hello, Alena!");
-Console.WriteLine("How are you!?");
 Console.ReadLine();
 
